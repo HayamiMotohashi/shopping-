0ff224748216cbec9d96ed6b740a23c7cd6b750b
hayami <hayami.taruto@gmail.com>
2018-11-02 17:38:52 +0900

    basic-login for users

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
+  devise_for :users
+
   root 'static_pages#index'
   get  '/index', to: 'static_pages#index'
   get  '/admin', to: 'static_pages#admin'
   get  '/order', to: 'static_pages#order'
   get  '/cart', to: 'static_pages#cart'
+  # get  '', to: 'static_pages#cart'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
